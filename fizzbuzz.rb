429b972c1eb770ed71ed9521cff661959e9608de
sebastian white <sebastian.c.white@nevada.unr.edu>
2021-03-11 03:06:32 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0
-    "Fizz"
+  puts  "Fizz"
   end
   if int % 5 == 0
-    "Buzz"
+  puts  "Buzz"
   end
 
 end
