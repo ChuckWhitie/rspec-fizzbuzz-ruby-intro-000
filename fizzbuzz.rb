e7e6f059f8a659355c147db44c762a6be8091b2d
sebastian white <sebastian.c.white@nevada.unr.edu>
2021-03-11 03:03:38 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 3 == 0
+  if int % 3 == 1
     "Fizz"
   end
-  if int % 5 == 0
+  if int % 5 == 1
     "Buzz"
   end
 
